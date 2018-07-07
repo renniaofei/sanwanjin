e67aa8523e361734a09c8089b433ca01e85a323b
niujunyang <11273539@qq.com>
2018-07-07 21:02:19 +0800

    t提交

---
+++ src/components/menu.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="menu">
+    <el-menu  :default-active="activeIndex" class="el-menu-demo" mode="horizontal">
+  <el-menu-item index="1">首页</el-menu-item>
+   <el-menu-item index="2">帮助</el-menu-item>
+    <el-menu-item index="3">充值</el-menu-item>
+     <el-menu-item index="4">邀请</el-menu-item>
+     <el-menu-item index="5">客服</el-menu-item>
+  <el-menu-item index="3" disabled>消息中心</el-menu-item>
+</el-menu>
+  </div>
+</template>
+<script>
+import { NavMenu } from "element-ui";
+export default {
+  name: "menu",
+  data() {
+    return {
+      activeIndex: "1",
+      activeIndex2: "1"
+    };
+  },
+  components: { NavMenu },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="index">
+        <my-menu></my-menu>
+        <div class="downInput">
+      <el-alert
+    title="三万金©平台目前支持包图网，千库网，千图网，90设计"
+    type="success"
+    :closable="false">
+  </el-alert>
+    <br>
+        <el-alert
+            title="警告提示的文案警告提示的文案警告提示的文案警告提示的文案警告提示的文案"
+            type="warning">
+        </el-alert>
+        <br>
+             <el-input v-model="input" class="fl el-input" placeholder="请输入下载链接"></el-input>  <el-button class="fl" type="primary">点击下载</el-button>
+        </div>
+    </div>
+</template>
+<script>
+import myMenu from "@/components/menu";
+export default {
+  data() {
+    return {
+      data: "1231",
+      input: ""
+    };
+  },
+  components: { myMenu }
+};
+</script>
+<style scoped>
+.index {
+  width: 1000px;
+  margin: 0 auto;
+}
+.downInput {
+  margin-top: 40px;
+}
+.fl {
+  float: left;
+}
+.el-input {
+  width: 500px;
+  margin-right: 20px;
+}
+</style>
